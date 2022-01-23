b83cfee980d3002463bdf573141570e3973a3f7e
MartinSarandev <mar7insarandev@gmail.com>
2022-01-23 10:37:08 +0200

    added a new sout in helloApp.java

--- src/main/java/com/example/events/HelloApplication.java
+++ src/main/java/com/example/events/HelloApplication.java
@@ -15,6 +15,8 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        System.out.println("its a demo using GIT");
     }
 
     public static void main(String[] args) {
